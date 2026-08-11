1912ef4cd337e7ed796edd712d0a5b40c2952ca9
infinity-117 <carlosandressiguenza15@gmail.com>
2026-08-10 23:02:47 -0400

    added groups for better organization

--- main.py
+++ main.py
@@ -6,11 +6,19 @@ from player import Player
 def main():
 
     pygame.init()
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0.0
+
+    #use groupings for easier management of updatable and drawable objects
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
+    # Main game loop
     while True:
         log_state()
 
@@ -23,16 +31,11 @@ def main():
 
         dt = clock.tick(60) / 1000
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
-        
-        pygame.display.flip()
-        
-    
+        updatable.update(dt)
+        for d in drawable:
+            d.draw(screen)
 
-    # print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    # print(f"Screen width: {SCREEN_WIDTH}")
-    # print(f"Screen height: {SCREEN_HEIGHT}")
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
