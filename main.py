b9c77b7f8531d631af1ca7372df035746b117978
infinity-117 <carlosandressiguenza15@gmail.com>
2026-08-10 22:14:17 -0400

    modified player class to include rotate methods and update in game loop to relfect rotation. Tested and working

--- main.py
+++ main.py
@@ -20,10 +20,11 @@ def main():
             
         for event in pygame.event.get():
             pass
+        dt = clock.tick(60) / 1000
         screen.fill("black")
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
-        dt = clock.tick(60) / 1000
         
     
 
